ec21d6de39e062adac60f5f29e4836712ba2e152
aradhya-gupta <aradhya.gupta0308@gmail.com>
2023-01-21 20:16:06 +0530

    Fix indentation

--- src/fetchtimeout.js
+++ src/fetchtimeout.js
@@ -1,9 +1,9 @@
-const fetch = require('node-fetch')
+const fetch = require("node-fetch");
 module.exports = function (url, options, timeout = 7000) {
-    return Promise.race([
-        fetch(url, options),
-        new Promise((_, reject) =>
-            setTimeout(() => reject(new Error('timeout init')), timeout)
-        )
-    ]);
-}
+  return Promise.race([
+    fetch(url, options),
+    new Promise((_, reject) =>
+      setTimeout(() => reject(new Error("timeout init")), timeout)
+    ),
+  ]);
+};
--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const fs = require("fs");
-const fetch = require('./fetchtimeout');
-const turf  = require('@turf/turf');
+const fetch = require("./fetchtimeout");
+const turf = require("@turf/turf");
 
 /**
  * MAPBOX TOKENS
  */
 
 //Mapbox Constants
-var MAPBOX_DOMAIN = "https://api.mapbox.com/"
-var ACCESS_TOKEN = "&access_token=pk.eyJ1Ijoia2F2eWEtMjQiLCJhIjoiY2w2Y2xhc2JpMW80MjNrcDNuZ3hwdDVxNSJ9.yRixm6cK2FVMVYiBk8AbDw";
+var MAPBOX_DOMAIN = "https://api.mapbox.com/";
+var ACCESS_TOKEN =
+  "&access_token=pk.eyJ1Ijoia2F2eWEtMjQiLCJhIjoiY2w2Y2xhc2JpMW80MjNrcDNuZ3hwdDVxNSJ9.yRixm6cK2FVMVYiBk8AbDw";
 
-
-var ISOCHRONE_QUERY_TYPE = "isochrone/v1/mapbox/driving/"
-var CONTOUR_QUERY = "?contours_minutes=60"
-var ISOCHRONE_OPTION = "&contours_colors=6706ce&polygons=true"
-
-
-
+var ISOCHRONE_QUERY_TYPE = "isochrone/v1/mapbox/driving/";
+var CONTOUR_QUERY = "?contours_minutes=60";
+var ISOCHRONE_OPTION = "&contours_colors=6706ce&polygons=true";
 
 const app = express();
 
@@ -31,99 +28,111 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("No Endpoint Found");
 });
 
+app.get("/getallstations", function (req, res) {
+  fs.readFile(
+    __dirname + "./../assets/" + "EV_STATION_DATA.json",
+    "utf8",
+    function (err, data) {
+      res.end(data);
+    }
+  );
+});
 
-app.get('/getallstations', function (req, res) {
-   fs.readFile( __dirname + "./../assets/" + "EV_STATION_DATA.json", 'utf8', function (err, data) {
-      res.end( data );
-   });
-})
+app.get("/getcurrentisochrone", function (req, res) {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
 
-app.get('/getcurrentisochrone', function(req, res){
-    const lat = req.query.lat;
-    const lon = req.query.lon;
+  let URL =
+    MAPBOX_DOMAIN +
+    ISOCHRONE_QUERY_TYPE +
+    lon +
+    "," +
+    lat +
+    CONTOUR_QUERY +
+    ISOCHRONE_OPTION +
+    ACCESS_TOKEN;
+  console.log(URL);
 
-        let URL = MAPBOX_DOMAIN + ISOCHRONE_QUERY_TYPE + lon + "," + lat + CONTOUR_QUERY + ISOCHRONE_OPTION + ACCESS_TOKEN
-        console.log(URL)
-       
-        fetch(URL, {method: 'GET'}, 15000) // throw after max 5 seconds timeout error
-                            .then(response => response.json())
-                            .then(response => {
-                                console.log(JSON.stringify(response))
-                                res.send(JSON.stringify(response))
+  fetch(URL, { method: "GET" }, 15000) // throw after max 5 seconds timeout error
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(JSON.stringify(response));
+      res.send(JSON.stringify(response));
+    })
+    .catch((e) => {
+      console.log("error");
+      console.log(e);
+      res.send("error");
+    });
+});
 
-                            } )
-                            .catch((e) => {
-                                console.log("error")
-                                console.log(e)
-                                res.send("error")
-                            })
-    
-})
+app.get("/getnearbystations", function (req, res) {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
 
+  let URL =
+    MAPBOX_DOMAIN +
+    ISOCHRONE_QUERY_TYPE +
+    lon +
+    "," +
+    lat +
+    CONTOUR_QUERY +
+    ISOCHRONE_OPTION +
+    ACCESS_TOKEN;
+  console.log(URL);
 
-app.get('/getnearbystations', function(req, res){
-    const lat = req.query.lat;
-    const lon = req.query.lon;
+  fetch(URL, { method: "GET" }, 15000) // throw after max 5 seconds timeout error
+    .then((response) => response.json())
+    .then((response) => {
+      // console.log(response.features[0].geometry.coordinates[0].length)
+      res.send(searchForStations(response.features[0].geometry.coordinates[0]));
+    })
+    .catch((e) => {
+      console.log("error");
+      console.log(e);
+      res.send("Timeout Error");
+    });
+});
 
-
-        let URL = MAPBOX_DOMAIN + ISOCHRONE_QUERY_TYPE + lon + "," + lat + CONTOUR_QUERY + ISOCHRONE_OPTION + ACCESS_TOKEN
-        console.log(URL)
-       
-        fetch(URL, {method: 'GET'}, 15000) // throw after max 5 seconds timeout error
-                            .then(response => response.json())
-                            .then(response => {
-                                // console.log(response.features[0].geometry.coordinates[0].length)
-                                res.send(searchForStations(response.features[0].geometry.coordinates[0]))
-
-                            } )
-                            .catch((e) => {
-                                console.log("error")
-                                console.log(e)
-                                res.send("Timeout Error")
-                            })
-    
-})
-
-
-function searchForStations(coordinates){
-
-  fs.readFile( __dirname + "./../assets/" + "EV_STATION_DATA.json", 'utf8', function (err, data) {
-      const evStationsObject = JSON.parse(data)
-      var evPoints = []
+function searchForStations(coordinates) {
+  fs.readFile(
+    __dirname + "./../assets/" + "EV_STATION_DATA.json",
+    "utf8",
+    function (err, data) {
+      const evStationsObject = JSON.parse(data);
+      var evPoints = [];
 
       for (var key in evStationsObject) {
         if (evStationsObject.hasOwnProperty(key)) {
-          // console.log([evStationsObject[key].position.lon,evStationsObject[key].position.lat]);   
-          evPoints.push([evStationsObject[key].position.lon,evStationsObject[key].position.lat]) 
+          // console.log([evStationsObject[key].position.lon,evStationsObject[key].position.lat]);
+          evPoints.push([
+            evStationsObject[key].position.lon,
+            evStationsObject[key].position.lat,
+          ]);
         }
       }
 
-      var polygonCoordinates = []
-      for(let i = 0; i < coordinates.length; i++){
-        polygonCoordinates.push([coordinates[i][0], coordinates[i][1]])
+      var polygonCoordinates = [];
+      for (let i = 0; i < coordinates.length; i++) {
+        polygonCoordinates.push([coordinates[i][0], coordinates[i][1]]);
       }
-    
-      
-      var points = turf.points(evPoints)
 
-      var searchWithin = turf.polygon([polygonCoordinates])
-  
+      var points = turf.points(evPoints);
+
+      var searchWithin = turf.polygon([polygonCoordinates]);
+
       var ptsWithin = turf.pointsWithinPolygon(points, searchWithin);
-      console.log(ptsWithin)
+      console.log(ptsWithin);
+    }
+  );
 
-      
-   });
-  
-  return "OK"
-
-
+  return "OK";
 }
 
-  
 app.listen(6001, () => {
-  console.log('listening on port 6001');
+  console.log("listening on port 6001");
 });
